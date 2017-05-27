b5cdef955ccc776033fcc25eb6712a9df902eca0
Lucas GOSSE <lgosse@e1r2p18.42.fr>
2017-05-27 16:28:25 +0200

    [ADD] Upgrade back-office

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -78,6 +78,7 @@ export class AppComponent implements OnInit {
     }
 
     login(): void {
+        console.log('logging in');
         this.route.queryParams.subscribe(params => {
             if (params.access_token) {
                 let accessToken = params.access_token;
