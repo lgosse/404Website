9ef4fbaa2a3caf0ef41794fadc9571603f7b515d
Lucas GOSSE <lgosse@e1r2p18.42.fr>
2017-05-28 10:17:51 +0200

    [UPD] Setting up dev database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,14 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyCcDclzNAmd5NrIK0w1mXrTKg6ABzMsDMQ',
-    authDomain: 'website-d0a07.firebaseapp.com',
-    databaseURL: 'https://website-d0a07.firebaseio.com',
-    storageBucket: 'website-d0a07.appspot.com',
-    messagingSenderId: '10707548851'
-  },
-  intraRedirectUrl: "https://api.intra.42.fr/oauth/authorize?client_id=0f82b31736baf6cecd13299061928f243962e0b8fce022aa7ba32a24ecc6c7b0&redirect_uri=https%3A%2F%2Fus-central1-website-d0a07.cloudfunctions.net%2Flogindev&response_type=code"
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBBebpFnoZJ0ZBWeB6cvN-F4mdVWf0nTHc",
+        authDomain: "dev-404website.firebaseapp.com",
+        databaseURL: "https://dev-404website.firebaseio.com",
+        projectId: "dev-404website",
+        storageBucket: "dev-404website.appspot.com",
+        messagingSenderId: "71979030642"
+    },
+    intraRedirectUrl: "https://api.intra.42.fr/oauth/authorize?client_id=0f82b31736baf6cecd13299061928f243962e0b8fce022aa7ba32a24ecc6c7b0&redirect_uri=https%3A%2F%2Fus-central1-website-d0a07.cloudfunctions.net%2Flogindev&response_type=code"
 };
